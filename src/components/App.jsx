1668fd67e6bb8b992e1b44d6b6b63e2ad7cb0eb3
MishynaIra <gortenzia1987@gmail.com>
2023-01-25 10:17:44 +0200

    feedback fixed switch case и разный стейт

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -82,52 +82,56 @@ import Notification from './Notification';
 
 
 export const App = () => {
-  const [buttons, setFeedbackButtons] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const onLeaveFeedback = name => {
-    setFeedbackButtons(prevState => ({...prevState, [name]: prevState[name] + 1}));
+    switch (name) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        return;
+    }
   };
 
   const countTotalFeedback = () => {
-
-      return buttons.good + buttons.neutral + buttons.bad;
+    return good + neutral + bad;
   };
 
-  const getButtonsNames = () => (Object.keys(buttons));
 
   const countPositiveFeedbackPercentage = () => {
-
-    if(buttons.good === 0) {
-      return 0;
+    return Math.ceil(Number((good * 100) / countTotalFeedback()));
     }
 
-    return Math.ceil(Number((buttons.good * 100) / countTotalFeedback()));
-  };
 
+  const getButtonsNames=['good', 'bad', 'neutral'];
   return (
       <div>
         <Section title='Please leave feedback'>
           <FeedbackOptions
-            options={getButtonsNames()}
+            options={getButtonsNames}
             onLeaveFeedback={onLeaveFeedback}
           />
         </Section>
         <Section title='Statistics'>
           {countTotalFeedback()
             ? <Statistics
-            good={buttons.good}
-            neutral={buttons.neutral}
-            bad={buttons.bad}
+            good={good}
+            neutral={neutral}
+            bad={bad}
             total={countTotalFeedback()}
             totalPositive={countPositiveFeedbackPercentage()}
             />
             : <Notification message="There is no feedback"/>
           }
-
         </Section>
 
     </div>
